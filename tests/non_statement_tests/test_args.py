d45e3010861f6b207a14917d8ffb374e26fcc69b
Paolo Zarpellon <paolozar@cisco.com>
2023-02-23 10:43:14 -0800

    Fix json dump test after output changes in previous commit

--- tests/non_statement_tests/test_args.py
+++ tests/non_statement_tests/test_args.py
@@ -10,14 +10,14 @@ def test_json_dump_arg():
     """
     parse_results = DDLParser(ddl).run(output_mode="hql", json_dump=True)
     expected = (
-        '[{"columns": [{"name": "key", "type": "STRING", "size": null, "references": null, '
-        '"unique": false, "nullable": true, "default": null, "check": null}, {"name": "value", '
-        '"type": "STRING", "size": null, "references": null, "unique": false, "nullable": true, '
-        '"default": null, "check": null}], "primary_key": [], "alter": {}, "checks": [], "index": [], '
-        '"partitioned_by": [], "tablespace": null, "stored_as": "DIRECTORIES", "location": null, "comment":'
-        ' null, "row_format": null, "fields_terminated_by": null, "lines_terminated_by": null,'
-        ' "map_keys_terminated_by": null, "collection_items_terminated_by": null, "external": false,'
-        ' "schema": null, "table_name": "list_bucket_single", '
+        '[{"table_name": "list_bucket_single", "schema": null, "partitioned_by": [], '
+        '"tablespace": null, "columns": [{"name": "key", "type": "STRING", "size": null, '
+        '"references": null, "unique": false, "nullable": true, "default": null, "check": null}, '
+        '{"name": "value", "type": "STRING", "size": null, "references": null, "unique": false, '
+        '"nullable": true, "default": null, "check": null}], "primary_key": [], "alter": {}, '
+        '"checks": [], "index": [], "stored_as": "DIRECTORIES", "location": null, "comment": null, '
+        '"row_format": null, "fields_terminated_by": null, "lines_terminated_by": null, '
+        '"map_keys_terminated_by": null, "collection_items_terminated_by": null, "external": false, '
         '"skewed_by": {"key": "key", "on": ["1", "5", "6"]}}]'
     )
     assert parse_results == expected
